a91221302ce44fc05ea208bfa2dded01acee0199
Girma Gebre <girmagebre555@gmail.com>
2025-10-26 14:42:51 +0300

    third commit

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,10 @@ const cors = require('cors');
 const express = require('express');
 const postRouter = require('./crude operation/router-post')
 const APP = express();
-// const path = "C:/Users/0/Desktop/Projects for remote jobs/Full stack web developer/Portifolio fullstack with mongoDB atlas/frontend" // frontend path for local
-const path = "https://portifolio-fullstack-with-mongodb-atlas-lc7n.onrender.com"// for deployed frontend url
-APP.use(express.static(path)); // keeping files in the public folder
+APP.use(express.static(__dirname + "../frontend")); // keeping files in the public folder
 const port = 2000;
 APP.use(cors())
-// APP.use(express.static); // keeping files in the public folder
+// APP.use(express.static); // keeping files in the public folder for local
 APP.use(express.json()); // to handle the json() caming data
 APP.use(express.urlencoded({extended: true})); 
 APP.use('/',postRouter); // enable the router to run
