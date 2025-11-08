d0659b2e3e7de9342be7628ab0c1701e6cf1b033
Girma Gebre <girmagebre555@gmail.com>
2025-11-08 12:58:20 +0300

    add autoincrement field on mongoDb database version 2

--- backend/crude operation/router-post.js
+++ backend/crude operation/router-post.js
@@ -8,23 +8,23 @@ mongoose.connect(process.env.MONGO_URL)
 .then(()=>console.log("Conneted to MongoDB Atlas"))
 .catch(err=>console.error('Connection failed', err))
 // create shema
-const employerSchema = mongoose.Schema({
+const employerSchema = new mongoose.Schema({
     name: {type: String, required: true},
     email: {type: String, required: true},
-    comment: {type: String, required: true}   
+    comment: {type: String, required: true}    
 }); 
  //---------insert "User_id" field and make autoincrement for each data inserted from client/fromtend-------
 //--------inc_field is plugin expexted object key
-employerSchema.plugIn(Autoincrement, {inc_field: "User_id"}); 
+employerSchema.plugin(Autoincrement, {inc_field: "UserId"}); 
 // creating "sideJobs" collection in the mongodb and class for creating an instance object template(data from client e.g: req.body).
-const employer = mongoose.model("sideJob", employerSchema);
+const employer = mongoose.model("sideJob", employerSchema); 
 
 router.post("/sidejob", async (req,res)=>{
     try{
     const nameNoExtraSpace = req.body.name.replace(/\s+/g, " ").trim(); //avoiding extra space from name from client/frontend  
-    const nameCaseInsensetive = new RegExp(nameNoExtraSpace, "i") // making variable (containing string) case insensetive using regex
+    const nameCaseInsensitive = new RegExp(nameNoExtraSpace, "i") // making variable (containing string) case insensetive using regex
     const {email, comment} = req.body;
-    const dataName = await employer.findOne({name:nameCaseInsensetive});
+    const dataName = await employer.findOne({name:nameCaseInsensitive});
     const dataEmail = await employer.findOne({email});
 
     //check both name and email are already exist first
